838b11bae72fcd97238956b8342fc4494534871b
KeslieBox <kesliebox@gmail.com>
2021-05-12 12:33:03 -0500

    commit tree 5eb4d1f0a63c6d8fdfa59491ac33d8ddbf8e58c2

--- src/Matrix.js
+++ src/Matrix.js
@@ -3,10 +3,12 @@ import React, { Component } from 'react';
 export default class Matrix extends Component {
   
   genRow = (vals) => {
+    console.log('v:', vals)
     return vals.map(val => <div className="cell"></div>) // replace me and render a cell component instead!
   }
   
   genMatrix = () => {
+    console.log(this)
     return this.props.values.map(rowVals => <div className="row">{this.genRow(rowVals)}</div>)
   }
   
@@ -18,4 +20,13 @@ export default class Matrix extends Component {
     )
   }
   
+  Matrix.defaultProps = {
+    values: (() => {
+      const row = ['#F00', '#F00', '#F00', '#F00', '#F00', '#F00', '#F00', '#F00', '#F00', '#F00']
+      const array = []
+      for(let i = 0; i <= 10; i++){
+        return array.push(row)
+      }
+    })
+  }
 }
